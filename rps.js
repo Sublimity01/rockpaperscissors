de3d9aed1b94077c5d831237a8104f8af28dcf1a
Sublimity01 <dougmhill@gmail.com>
2022-07-09 18:22:45 -0600

    Make rps a webpage and first to five

--- rps.js
+++ rps.js
@@ -1,13 +1,61 @@
-function game() {
+
+
     let compWins = 0;
     let playWins = 0;
     let ties = 0;
-    for (let i = 0; i < 5; i++) {
+    let outcome = "";
+
+    const results = document.querySelector('#results');
+    const outcomes = document.createElement('p');
+    const playerResults = document.createElement('p');
+    playerResults.style.cssText = 'color: green;';
+    const compResults = document.createElement('p');
+    compResults.style.cssText = 'color: red;';
+    const tieResults = document.createElement('p');
+    results.appendChild(outcomes);
+    results.appendChild(playerResults);
+    results.appendChild(compResults);
+    results.appendChild(tieResults);
+
+
+ function winCondition () {
+     };
+
+//receive player input as selection and initialize game
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            game(button.id);
+        });
+    });
+
+    /*
+    const rockbtn = document.querySelector('#rockbtn');
+    rockbtn.addEventListener('click', () => {
+      game("rock");
+    });
+    const paperbtn = document.querySelector('#paperbtn');
+    paperbtn.addEventListener('click', () => {
+        game("paper");
+    });
+    const scissorsbtn = document.querySelector('#scissorsbtn');
+    scissorsbtn.addEventListener('click', () => {
+        game("scissors");
+    });
+    */
+    
+function game(playerSelect) {
+
+    
+//    for (let i = 0; i < 5; i++) {
 //initialize variables for computer selection and player selection
-let playerSelection;
+let playerSelection = playerSelect;
 let computerSelection;
 //receive player input as player selection
-playerSelection = prompt("Choose your fighter! Rock, Paper, or Scissors?");
+//playerSelection = prompt("Choose your fighter! Rock, Paper, or Scissors?");
+
+
+
 //transform case of player selection to lower case -- this ensures case insensitivity of player input
 playerSelection = playerSelection.toLowerCase();
 console.log(`You chose ${playerSelection}`)
@@ -80,35 +128,56 @@ console.log(`The result is ${result}`);
 function capFirst(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
+
 //output result of comparison as string
 if (result === "invalid") {
-    return " Your selection is not a viable fighter. Please try again. There are... there are only three options I don't know how you messed up this badly.";
+    outcome = "Your selection is not a viable fighter. Please try again. There are... there are only three options I don't know how you messed up this badly.";
 }
 else if (result === "player") {
     playWins = ++playWins;
-    return `You Win! ${capFirst(playerChoice)} beats ${capFirst(computerChoice)}!`;
+    outcome = `You Win! ${capFirst(playerChoice)} beats ${capFirst(computerChoice)}!`;
 }
 else if (result === "comp") {
     compWins = ++compWins;
-    return `You chose... poorly. ${capFirst(computerChoice)} beats ${capFirst(playerChoice)}.`;
+    outcome = `You chose... poorly. ${capFirst(computerChoice)} beats ${capFirst(playerChoice)}.`;
 }
 else if (result === "tie") {
     ties = ++ties;
-    return `You both selected ${capFirst(playerChoice)}. There is no victory here, nor defeat, just a gray void of empty meaninglessness.`;
+    outcome = `You both selected ${capFirst(playerChoice)}. There is no victory here, nor defeat, just a gray void of empty meaninglessness.`;
 }
 else if (result === "explode") {
-    return `Oh dear. The computer has used its incredible processing power to overwrite the rules and select Dynamite, which has unfortunately also caused it to explode. We'll need to work on that.`;
+    outcome = `Oh dear. The computer has used its incredible processing power to overwrite the rules and select Dynamite, which has unfortunately also caused it to explode. We'll need to work on that.`;
 }
 else {
-    return `We're not sure what happened here. Please hold.`
+    outcome = `We're not sure what happened here. Please hold.`
 }
 };
 console.log(playRound(playerSelection, computerSelection));
 console.log(playWins + " " + compWins + " " + ties);
 
-    }
-//output final results
+ //   }
+/*output final results
 return `Your victories: ${playWins}
 Computer victories: ${compWins}
-Boring ties: ${ties}`;
+Boring ties: ${ties}`;*/
+
+if (compWins == 5) {
+    alert(`You have failed. The computer has won ${compWins} victories to your paltry ${playWins}. Refresh to try again.`);
+}
+else if (playWins == 5) {
+    alert(`Congratulations on your triumph! You have emerged victorious ${playWins} times while suffering only ${compWins} defeats. We'll ignore the ${ties} ties. 
+    Refresh to press your luck and try again.`);
+};
+if (playWins == 5 || compWins == 5) {
+    compWins = 0;
+    playWins = 0;
+    ties = 0;
+};
+
+outcomes.textContent = outcome;
+playerResults.textContent = 'Your victories: ' + playWins;
+compResults.textContent = `Computer victories: ${compWins}`;
+tieResults.textContent = `Boring ties: ${ties}`;
+
+
 }
